d19448f9c3c38a43d14f58ecf22dfa2db39f96e9
yesser bakhouch <yesserbakhouch@hotmail.com>
2023-10-31 15:49:43 +0100

    authentification

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {ProductsComponent} from "./products/products.component";
 import {CustomersComponent} from "./customers/customers.component";
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
-  {
-    path : "products" ,
-    component : ProductsComponent },
-  {
-    path:"customers" ,
-    component :CustomersComponent
-  }
+  {path: "login", component: LoginComponent},
+  {path: "", component: LoginComponent},
+  {path: "products", component: ProductsComponent},
+  {path: "customers", component: CustomersComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
